070d54f85a0a53c6ac2bc9363f3c9945933ac911
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-01-09 23:40:48 +0200

    setup mobile navigation

--- imports/ui/PrivateHeader.js
+++ imports/ui/PrivateHeader.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Accounts} from "meteor/accounts-base";
+import {Session} from "meteor/session";
 import PropTypes from 'prop-types';
 // UNCOMMENT IF YOU WANT TO USE THE CREATECONTAINER FUNC...
 //import {createContainer} from "meteor/react-meteor-data"
@@ -7,9 +8,12 @@ import {withTracker} from "meteor/react-meteor-data";
 
 
 export const PrivateHeader = (props) => {
+    const navImgSource = props.isNavOpen ? '/images/close.svg' : '/images/menu.svg';
+
     return (
             <div className="header">
                 <div className="header__content">
+                    <img onClick={props.handleToggle} src={navImgSource}/>
                     <h1 className="header__title"> {props.title}</h1>
                     <button className="header__content--button" onClick={() => props.handleLogout()}>Logout</button>
                 </div>
@@ -20,7 +24,9 @@ export const PrivateHeader = (props) => {
 
 PrivateHeader.propTypes = {
     title: PropTypes.string.isRequired,
-    handleLogout: PropTypes.func.isRequired
+    handleLogout: PropTypes.func.isRequired,
+    isNavOpen: PropTypes.bool.isRequired,
+    handleToggle: PropTypes.func.isRequired
 };
 
 PrivateHeader.defaultProps = {
@@ -38,6 +44,9 @@ PrivateHeader.defaultProps = {
 //WORKS BUT DOES NOT GIVE A WARNING - THE SUGGESTED SOLUTION FROM THE WARNING
 export default withTracker( () => {
     return {
-        handleLogout:() =>  Accounts.logout()
+        handleLogout:() =>  Accounts.logout(),
+        handleToggle: () => Session.set('isNavOpen', !Session.get('isNavOpen')),
+        isNavOpen: Session.get('isNavOpen')
+
     };
 }) (PrivateHeader);
